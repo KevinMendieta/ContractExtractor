30eadf54749bf8fab757f3b9eccdbdb23db57ab0
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-03-28 20:49:01 -0500

    First Solve

--- extract.py
+++ extract.py
@@ -35,16 +35,58 @@ def getResponse(url, params):
 		url: URL for request.
 		param: params for the Web Service consume.
 	Return:
-		The text of the response ()
+		The text of the response (string)
 	"""
 	return requests.get(url, params = params).text
+
+def divideDate(start, finish):
+	"""
+	Calculate the mid date from two dates.
+	Args:
+		start: the first date.
+		finish: the second date.
+	Return:
+		The middle date from two dates.
+	"""
+	sPrefixes = start.split("-")
+	fPrefixes = finish.split("-")
+	if ((int(sPrefixes[2]) + int(fPrefixes[2])) >> 1 == int(sPrefixes[2])):
+		return sPrefixes[0] + "-" + fPrefixes[1] + "-" +str(int(fPrefixes[2]) >> 1)
+	else:
+		return sPrefixes[0] + "-" + str((int(sPrefixes[1]) + int(fPrefixes[1])) >> 1) + "-" + str((int(sPrefixes[2]) + int(fPrefixes[2])) >> 1)
+
+
+def extractContracts(start, finish):
+	"""
+	Extract the contracts from two dates, in case that the number of contracts became greater
+	than 100 the dates get sliced by the middle, and re-calculate the contracts with the new
+	dates.
+	Args:
+		start: the first date.
+		finish: the second date.
+	Return:
+		The total of contracts between two dates.
+	Global:
+		url: The url to consume the web service.
+		id: The id for consume the web service.
+		calls: Number of calls for solve the problem.
+	"""
+	global url, id, calls
+	data = getResponse(url, buildParams(id, start, finish))
+	calls += 1
+	if (data == '"Hay mÃ¡s de 100 resultados"'):
+		middle = sliceDate(start, finish)
+		return int(extractContracts(start, middle)) + int(extractContracts(middle, finish))
+	else:
+		return int(data)
 
 def main():
 	"""
 	main function
 	"""
-	global id, start, finish, url
-	print(getResponse(url, buildParams(id, start, finish))
+	global start, finish, calls
+	contracts = extractContracts(start, finish)
+	print("Calculated: " + str(contracts) + " contracts from " + start + " to " + finish + " in: " + str(calls) + " call/s.")
 
 
 main()
