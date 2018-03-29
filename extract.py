c16f7154216ab6121f3dc40fa6425534eab84202
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-03-28 20:30:15 -0500

    Refactor Connection

--- extract.py
+++ extract.py
@@ -1,4 +1,4 @@
-#author: KevinMendieta
+#@author: KevinMendieta
 #libraries
 from datetime import date
 import requests
@@ -12,14 +12,39 @@ start = "2017-01-01"
 finish = "2017-12-31"
 calls = 0
 
-def main():
-	global id, start, finish, url
-	params = (
+def buildParams(id, start, finish):
+	"""
+	Build parameters for connection.
+	Args:
+		id: id parameter.
+		start: the start date parameter.
+		finish: the finish date parameter.
+	Returns:
+		a tuple with the parameters inside.
+	"""
+	return (
 		("id", id),
 		("start", start),
 		("finish", finish)
 	)
-	print(requests.get(url, params = params).text)
+
+def getResponse(url, params):
+	"""
+	Get the response for the request to the Web Service.
+	Args:
+		url: URL for request.
+		param: params for the Web Service consume.
+	Return:
+		The text of the response ()
+	"""
+	return requests.get(url, params = params).text
+
+def main():
+	"""
+	main function
+	"""
+	global id, start, finish, url
+	print(getResponse(url, buildParams(id, start, finish))
 
 
 main()
